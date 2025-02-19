a34dbc6dc3756a9326df76e3b809458a2801cabb
RootSasha <sashamankovsky2019@gmail.com>
2025-02-19 13:30:42 +0000

    Налаштований сайт

--- seting/plugin.sh
+++ seting/plugin.sh
@@ -2,7 +2,7 @@
 
 JENKINS_URL="http://192.168.0.113:8080"
 JENKINS_USER="admin"
-JENKINS_PASSWORD="1"  # Увага: краще використовувати API токен
+JENKINS_PASSWORD="1" # Увага: краще використовувати API токен
 
 # Download jenkins-cli.jar if it doesn't exist
 if [ ! -f "jenkins-cli.jar" ]; then
@@ -15,40 +15,43 @@ if [ ! -f "jenkins-cli.jar" ]; then
 fi
 
 plugins=(
-    "cloudbees-folder"
-    "custom-markup-formatter"  # Замінено "owasp-markup-formatter"
-    "build-timeout"
-    "credentials-binding"
-    "timestamper"
-    "ws-cleanup"
-    "ant"
-    "gradle"
-    "workflow-aggregator"
-    "github-branch-source"
-    "github-api"
-    "pipeline-github-lib"
-    "pipeline-graph-view"
-    "git"
-    "ssh-slaves"
-    "matrix-auth"
-    "pam-auth"
-    "ldap"
-    "email-ext"
-    "mailer"
-    "dark-theme"
+  "cloudbees-folder"
+  "custom-markup-formatter"
+  "build-timeout"
+  "credentials-binding"
+  "timestamper"
+  "ws-cleanup"
+  "ant"
+  "gradle"
+  "workflow-aggregator"
+  "github-branch-source"
+  "github-api"
+  "pipeline-github-lib"
+  "pipeline-graph-view"
+  "git"
+  "ssh-slaves"
+  "matrix-auth"
+  "pam-auth"
+  "ldap"
+  "email-ext"
+  "mailer"
+  "dark-theme"
 )
 
 for plugin in "${plugins[@]}"; do
-    echo "Installing $plugin..."
-    java -jar jenkins-cli.jar -s "$JENKINS_URL" -auth "$JENKINS_USER:$JENKINS_PASSWORD" install-plugin "$plugin"
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to install $plugin. Skipping..."
-    fi
+  echo "Installing $plugin..."
+  java -jar jenkins-cli.jar -s "$JENKINS_URL" -auth "$JENKINS_USER:$JENKINS_PASSWORD" install-plugin "$plugin"
+  if [[ $? -ne 0 ]]; then
+    echo "Failed to install $plugin. Skipping..."
+  fi
 done
 
+echo "🧹 Очищуємо кеш Jenkins для застосування змін..."
+sudo rm -rf /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+sudo rm -rf /var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion
+
+echo "RUNNING" | sudo tee /var/lib/jenkins/jenkins.install.UpgradeWizard.state > /dev/null
+
+echo "🔄 Перезапуск Jenkins..."
 sudo systemctl restart jenkins
 
-echo "Jenkins restart initiated."
-
-echo "Create credentials"
-bash seting/cred.sh
